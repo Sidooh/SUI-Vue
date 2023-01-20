1fb4d2636fd5e94fe6bcee6caad6fe559ff6d740
Nabcellent <nabcellent.dev@gmail.com>
2023-01-20 16:03:06 +0300

    Removes tan-stack table helpers

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,3 @@ export * from './enums';
 export * from './helpers';
 export * from './logger';
 export * from './types';
-
-export { createColumnHelper } from '@tanstack/vue-table'
-export type { CellContext } from '@tanstack/vue-table'
