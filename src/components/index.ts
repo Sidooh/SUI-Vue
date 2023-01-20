a09a67c25968f738051ef560cc6f1732c32607d5
Nabcellent <nabcellent.dev@gmail.com>
2023-01-20 10:24:51 +0300

    Adds types

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 export { default as Badge } from './Badge.vue'
 export { default as DataTable } from './datatable/DataTable.vue'
 export { default as Logo } from './Logo.vue'
-export { default as Phone } from './Phone.vue'
+export { default as PhoneBadge } from './Phone.vue'
 export { default as StatusBadge } from './StatusBadge.vue'
 export { default as TableDate } from './TableDate.vue'
 export { default as Tooltip } from './Tooltip.vue'
---
+++ src/utils/types.ts
@@ -0,0 +1,51 @@
+import { IconDefinition } from "@fortawesome/free-regular-svg-icons";
+import { Status } from "./enums";
+
+type RouteChildType = {
+    name: string
+    to: string
+    active?: boolean
+    icon?: IconDefinition
+    children?: RouteChildType[]
+}
+export type RouteType = {
+    label?: string
+    children: RouteChildType[]
+}
+
+type Model = {
+    id?: number
+    created_at?: string
+    updated_at?: string
+}
+export type User = Model & {
+    id: number
+    name: string
+    email: string
+    username: string
+    id_number: number
+    status: Status
+    password: string
+}
+
+
+export type Account = Model & {
+    id: number
+    phone: string
+    active: boolean
+    inviter_id: number
+    user_id: number
+    user: User
+    inviter: Invite
+    invite_code: string
+    level: number
+}
+
+export type Invite = Model & {
+    id: number
+    phone: string
+    status: Status
+    inviter_id: number
+    account_id: number
+    user: User
+}
