c9cde9c6e7d9206379e9c25092e153641474b0f3
Nabcellent <nabcellent.dev@gmail.com>
2023-01-20 15:59:26 +0300

    Exports helpers

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,6 @@ export * from './enums';
 export * from './helpers';
 export * from './logger';
 export * from './types';
+
+export { createColumnHelper } from '@tanstack/vue-table'
+export type { CellContext } from '@tanstack/vue-table'
