c83f8920f36d005108d16740aa880c353914e85a
Nabcellent <nabcellent.dev@gmail.com>
2023-08-17 09:42:47 +0300

    Updates package build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { URL } from 'node:url'
 import { resolve } from 'path'
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()], // to process SFC
+    plugins: [vue(), dts()], // to process SFC
     resolve: {
         alias: {
             "@/": new URL("./src/", import.meta.url).pathname,
