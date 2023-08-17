b5a39c6614a670024609c756213adfb89c682966
Nabcellent <nabcellent.dev@gmail.com>
2023-08-17 08:33:54 +0300

    Updates package build

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
-import { App } from 'vue'
-import * as components from './components'
-
 import './assets/css/theme.min.css';
 import './assets/css/user.min.css';
-
-export default {
-    install: (app: App) => {
-        for (const key in components) {
-            // @ts-expect-error
-            app.component(key, components[key])
-        }
-    }
-}
+//
+// export default {
+//     install: (app: App) => {
+//         for (const key in components) {
+//             // @ts-expect-error
+//             app.component(key, components[key])
+//         }
+//     }
+// }
 
 export * from './components'
 export * from './utils'
